7b60f92c91069978355e5d82e8d569ab80a92b90
Sirahxyz <harisafifudin123@gmail.com>
2023-10-29 12:35:32 +0700

    Commit pertama

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php 
+    $a = 'Saya Sedang Belajar php' ;
+    $b = 5;
+
+    print $a;
+    echo $b;
+?>
+
+<?php 
+    $a = 5;
+    $b = "Sedang belajar php $a";
+
+    echo $b;
+?>
+
+<?php
+    Echo "Hello World";
+    Echo "Hello World";
+    Echo "Hello World";
+?>
+
+
+<?php
+    $Haris ="Haris";
+    echo $Haris;
+
+?>
+
+
+
+//Penulisan Tipe data interger
+<?php 
+    $umur = 21;
+    $harga = 15000;
+    $rugi = 500000;
+
+    echo $umur; 
+    echo "\n";
+    echo $harga;
+    echo "\n";
+    echo $rugi ; 
+?>
+
+<?php 
+    $a = 14;
+    $b = 16;
+    $c = $a + $b;
+    echo $c;
+    echo"\n";
+    $d = $a * $b ;
+    echo $d;
+?>
+
+
+//Penulisan Tipe data Float
+<?php
+    $angka_float1 = 0.78;
+    $angka_float2 = 14.99;
+    $angka_scinetific1 =0.314E1;
+    $angka_scinetific2 =0.3365E-3;
+
+    echo $angka_float1;
+    echo "\n";
+    echo $angka_float2;
+    echo "\n";
+    echo $angka_scinetific1;
+    echo "\n";
+    echo $angka_scinetific2;
+?>
+
+<?php
+    $a = 10.66;
+    $b = 12.4;
+    $c = $a + $b;
+
+    echo $c;
+    
+    $d = $a * $b;
+    echo $d;
+?>
+
+
+//Penulisan tipe data String
+<?php 
+    $String1 = 'Ini adalah nama string sederhana';
+    $String2 = "Ini adalah string yang bisa memeiliki beberapa baris";
+    $String3 = 'Dia Berkata : "I\'ll be back';
+
+    echo $String1; 
+    echo "\n";
+    echo $String2;
+    echo "\n";
+    echo $String3;
+    echo "\n";
+?>
+
+//Boolean
+    <?php
+    $benar=true;
+    $salah =false;
+
+    echo "benar = $benar, salah = $salah ";
+?>
+
+
+//Operator aritmatika
+<?php
+    $hasil1 = -3;
+    $hasil2 = 3+5;
+    $hasil3 = 8-4.5;
+    $hasil4 = 2*5;
+    $hasil5 = 3+8/5-3;
+    $hasil6 = 10 % 4;
+
+    echo "$hasil1:"; var_dump($hasil1);
+    echo "\n";
+    echo "$hasil2:"; var_dump($hasil1);
+    echo "\n";
+    echo "$hasil3:"; var_dump($hasil1);
+    echo "\n";
+    echo "$hasil4:"; var_dump($hasil1);
+    echo "\n";
+    echo "$hasil5:"; var_dump($hasil1);
+    echo "\n";
+    echo "$hasil6:"; var_dump($hasil1);
+?>
